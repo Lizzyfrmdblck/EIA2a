82b6567366e3792bb3a1df8bca4a062e03645eef
Lizzyfrmdblck <elyssia-sofie.duerr@hs-furtwangen.de>
2019-04-07 18:42:08 +0200

    Konzept Aufgabe02

--- aufgabe02/main.js
+++ aufgabe02/main.js
@@ -9,130 +9,162 @@ Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde n
 let karo7 = {
     zahl: "7",
     farbe: "Karo",
+    img: ""
 };
 let herz7 = {
     zahl: "7",
     farbe: "Herz",
+    img: ""
 };
 let pik7 = {
     zahl: "7",
     farbe: "Pik",
+    img: ""
 };
 let kreuz7 = {
     zahl: "7",
     farbe: "Kreuz",
+    img: ""
 };
 let karo8 = {
     zahl: "8",
     farbe: "Karo",
+    img: ""
 };
 let herz8 = {
     zahl: "8",
     farbe: "Herz",
+    img: ""
 };
 let pik8 = {
     zahl: "8",
     farbe: "Pik",
+    img: ""
 };
 let kreuz8 = {
     zahl: "8",
     farbe: "Kreuz",
+    img: ""
 };
 let karo9 = {
     zahl: "9",
     farbe: "Karo",
+    img: ""
 };
 let herz9 = {
     zahl: "9",
     farbe: "Herz",
+    img: ""
 };
 let pik9 = {
     zahl: "9",
     farbe: "Pik",
+    img: ""
 };
 let kreuz9 = {
     zahl: "9",
-    farbe: "Kreuz"
+    farbe: "Kreuz",
+    img: ""
 };
 let karo10 = {
     zahl: "10",
     farbe: "Karo",
+    img: ""
 };
 let herz10 = {
     zahl: "10",
     farbe: "Herz",
+    img: ""
 };
 let pik10 = {
     zahl: "10",
     farbe: "Pik",
+    img: ""
 };
 let kreuz10 = {
     zahl: "10",
     farbe: "Kreuz",
+    img: ""
 };
 let karoBube = {
     zahl: "Bube",
     farbe: "Karo",
+    img: ""
 };
 let herzBube = {
     zahl: "Bube",
     farbe: "Herz",
+    img: ""
 };
 let pikBube = {
     zahl: "Bube",
     farbe: "Pik",
+    img: ""
 };
 let kreuzBube = {
     zahl: "Bube",
     farbe: "Kreuz",
+    img: ""
 };
 let karoDame = {
     zahl: "Dame",
     farbe: "Karo",
+    img: ""
 };
 let herzDame = {
     zahl: "Dame",
     farbe: "Herz",
+    img: ""
 };
 let pikDame = {
     zahl: "Dame",
     farbe: "Pik",
+    img: ""
 };
 let kreuzDame = {
     zahl: "Dame",
     farbe: "Kreuz",
+    img: ""
 };
 let karoKoenig = {
     zahl: "Koenig",
     farbe: "Karo",
+    img: ""
 };
 let herzKoenig = {
     zahl: "Koenig",
     farbe: "Herz",
+    img: ""
 };
 let pikKoenig = {
     zahl: "Koenig",
     farbe: "Pik",
+    img: ""
 };
 let kreuzKoenig = {
     zahl: "Koenig",
     farbe: "Kreuz",
+    img: ""
 };
 let karoAss = {
     zahl: "Ass",
     farbe: "Karo",
+    img: ""
 };
 let herzAss = {
     zahl: "Ass",
     farbe: "Herz",
+    img: ""
 };
 let pikAss = {
     zahl: "Ass",
     farbe: "Pik",
+    img: ""
 };
 let kreuzAss = {
     zahl: "Ass",
     farbe: "Kreuz",
+    img: ""
 };
 let allCards = [karo7, karo8, karo9, karo10, karoBube, karoDame, karoKoenig, karoAss, herz7, herz8, herz9, herz10, herzBube, herzDame, herzKoenig, herzAss, pik7, pik8, pik9, pik10, pikBube, pikDame, pikKoenig, pikAss, kreuz7, kreuz8, kreuz9, kreuz10, kreuzBube, kreuzDame, kreuzKoenig, kreuzAss];
 let ziehStapel = [];
--- aufgabe02/main.ts
+++ aufgabe02/main.ts
@@ -10,157 +10,190 @@ Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde n
 interface Karten {
     zahl: string;
     farbe: string;
+    img: string;
 }
 
 let karo7: Karten = {
     zahl: "7",
     farbe: "Karo",
+    img: ""
 }
 
 let herz7: Karten = {
     zahl: "7",
     farbe: "Herz",
+    img: ""
 }
 
 
 let pik7: Karten = {
     zahl: "7",
     farbe: "Pik",
+    img: ""
 }
 
 let kreuz7: Karten = {
     zahl: "7",
     farbe: "Kreuz",
+    img: ""
 
 }
 let karo8: Karten = {
     zahl: "8",
     farbe: "Karo",
+    img: ""
 
 }
 let herz8: Karten = {
     zahl: "8",
     farbe: "Herz",
+    img: ""
 
 }
 let pik8: Karten = {
     zahl: "8",
     farbe: "Pik",
+    img: ""
 
 }
 let kreuz8: Karten = {
     zahl: "8",
     farbe: "Kreuz",
+    img: ""
 
 }
 let karo9: Karten = {
     zahl: "9",
     farbe: "Karo",
+    img: ""
 }
 
 let herz9: Karten = {
     zahl: "9",
     farbe: "Herz",
+    img: ""
 }
 
 let pik9: Karten = {
     zahl: "9",
     farbe: "Pik",
+    img: ""
 }
 
 let kreuz9: Karten = {
     zahl: "9",
-    farbe: "Kreuz"
+    farbe: "Kreuz",
+    img: ""
 }
 
 let karo10: Karten = {
     zahl: "10",
     farbe: "Karo",
+    img: ""
 
 }
 let herz10: Karten = {
     zahl: "10",
     farbe: "Herz",
+    img: ""
 
 }
 let pik10: Karten = {
     zahl: "10",
     farbe: "Pik",
+    img: ""
 
 }
 let kreuz10: Karten = {
     zahl: "10",
     farbe: "Kreuz",
+    img: ""
 
 }
 let karoBube: Karten = {
     zahl: "Bube",
     farbe: "Karo",
+    img: ""
 
 }
 let herzBube: Karten = {
     zahl: "Bube",
     farbe: "Herz",
+    img: ""
 
 }
 let pikBube: Karten = {
     zahl: "Bube",
     farbe: "Pik",
+    img: ""
 
 }
 let kreuzBube: Karten = {
     zahl: "Bube",
     farbe: "Kreuz",
+    img: ""
 
 }
 let karoDame: Karten = {
     zahl: "Dame",
     farbe: "Karo",
+    img: ""
 
 }
 let herzDame: Karten = {
     zahl: "Dame",
     farbe: "Herz",
+    img: ""
 }
 let pikDame: Karten = {
     zahl: "Dame",
     farbe: "Pik",
+    img: ""
 }
 let kreuzDame: Karten = {
     zahl: "Dame",
     farbe: "Kreuz",
+    img: ""
 }
 let karoKoenig: Karten = {
     zahl: "Koenig",
     farbe: "Karo",
+    img: ""
 }
 let herzKoenig: Karten = {
     zahl: "Koenig",
     farbe: "Herz",
+    img: ""
 }
 let pikKoenig: Karten = {
     zahl: "Koenig",
     farbe: "Pik",
+    img: ""
 }
 let kreuzKoenig: Karten = {
     zahl: "Koenig",
     farbe: "Kreuz",
+    img: ""
 }
 let karoAss: Karten = {
     zahl: "Ass",
     farbe: "Karo",
+    img: ""
 }
 let herzAss: Karten = {
     zahl: "Ass",
     farbe: "Herz",
+    img: ""
 }
 let pikAss: Karten = {
     zahl: "Ass",
     farbe: "Pik",
+    img: ""
 }
 let kreuzAss: Karten = {
     zahl: "Ass",
     farbe: "Kreuz",
+    img: ""
 }
 
 let allCards: Karten[] = [karo7, karo8, karo9, karo10, karoBube, karoDame, karoKoenig, karoAss, herz7, herz8, herz9, herz10, herzBube, herzDame, herzKoenig, herzAss, pik7, pik8, pik9, pik10, pikBube, pikDame, pikKoenig, pikAss, kreuz7, kreuz8, kreuz9, kreuz10, kreuzBube, kreuzDame, kreuzKoenig, kreuzAss];
